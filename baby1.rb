a6064a89cd8842a91ea4b2ffef505ffa0623fd75
Oge Nnadi <oge@nnadi.org>
2018-07-09 16:47:16 -0500

    Disallow start times coming after end times

--- baby1.rb
+++ baby1.rb
@@ -5,12 +5,17 @@ def nightly_charge(start_time, end_time)
   adjusted_start = start_time + hours_to_seconds(7)
   adjusted_end   = end_time   + hours_to_seconds(7)
   
-  if adjusted_start.hour % 24 > 11
+  if adjusted_start.hour  > 11
     raise "Started before 5:00PM"
   end
 
-  if adjusted_end.hour % 24 > 11
+  if adjusted_end.hour  > 11
     raise "Ended after 4:00AM"
+  end
+
+
+  if adjusted_start.hour > adjusted_end.hour
+    raise "Start time occurs after the end time"
   end
 end
 
@@ -35,6 +40,12 @@ describe "The nightly charge" do
   it "is invalid if work ends outside work hours" do   
     lambda {
       nightly_charge @three_am, @five_am
+    }.must_raise StandardError
+  end
+
+  it "is invalid if end time comes before start time" do
+    lambda {
+      nightly_charge @three_am, @six_pm
     }.must_raise StandardError
   end
 
