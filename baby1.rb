61f2572901206730ecf5d22e2d5558bf07767067
Oge Nnadi <oge@nnadi.org>
2018-07-10 10:36:37 -0500

    Implement pre-bedtime to pre-midnight rate

--- baby1.rb
+++ baby1.rb
@@ -4,30 +4,44 @@ require "time"
 
 class Babysitter
   SB_RATE = 12
-  BEDTIME = 21
+  BM_RATE = 8
+  ME_RATE = 16
+
+  BEDTIME  = 21
+  MIDNIGHT = 24
+  WORK_END = 4
   
   # Returns the charge for working from START_TIME to END_TIME.
   #
   # START_TIME and END_TIME must be between the working hours, and START_TIME
   # must come before END_TIME.
   def nightly_charge(start_time, end_time)
-    adjusted_start = (start_time + 7) % 24
-    adjusted_end   = (end_time   + 7) % 24
-    
-    if adjusted_start  > 11
+    if adjusted(start_time) > 11
       raise "Started before 5:00PM"
     end
 
-    if adjusted_end  > 11
+    if adjusted(end_time) > 11
       raise "Ended after 4:00AM"
     end
 
-
-    if adjusted_start > adjusted_end
+    if adjusted(start_time) > adjusted(end_time)
       raise "Start time occurs after the end time"
     end
 
-    12
+    if adjusted(start_time) < adjusted(BEDTIME) &&
+       adjusted(end_time)   <= adjusted(BEDTIME)
+      (adjusted(end_time) - adjusted(start_time))* SB_RATE
+    elsif adjusted(start_time) < adjusted(BEDTIME) &&
+          adjusted(end_time)   <= adjusted(MIDNIGHT)
+      (adjusted(BEDTIME) - adjusted(start_time)) * SB_RATE +
+      (adjusted(end_time) - adjusted(BEDTIME))   * BM_RATE  
+    end
+  end
+
+
+  private
+  def adjusted(hour)
+    (hour + 7) % 24
   end
 end
 
@@ -46,7 +60,9 @@ describe "The nightly charge" do
     @five_am    = 5
 
     @sb_rate = Babysitter::SB_RATE
+    @bm_rate = Babysitter::BM_RATE
     @bedtime = Babysitter::BEDTIME
+    @midnight = 24
   end
   
   it "is invalid if work starts outside work hours" do
@@ -72,5 +88,12 @@ describe "The nightly charge" do
     e = @bedtime - 1
     
     @bs.nightly_charge(s, e).must_equal @sb_rate*(e-s)
+  end
+
+  it "Should pay SB and BM rates if work starts before bedtime and ends after bedtime but before midnight" do
+    s = @bedtime - 1
+    e = @midnight
+
+    @bs.nightly_charge(s, e).must_equal @sb_rate + (@midnight - @bedtime)*@bm_rate
   end
 end
